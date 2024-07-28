4839d0dee1eb30db76c1f977ac5669f8a8390f00
manugowda18 <100668132+manugowda18@users.noreply.github.com>
2024-07-28 15:17:35 +0530

    work update

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,66 +1,33 @@
 export const Work = [
   {
-    id: 3,
-    name: "PVL-Labs",
+    id: 2,
+    name: "KP Consulting",
     description:
-      "Web3 Developer - Solidity",
+      "FullStack Blockchain dev",
+    tags: ["Blockchain","Solidity", "MERN","Team Management"],
+  },
+  {
+    id: 2,
+    name: "Entersoft Security",
+    description:
+      "Smart Contract Auditor",
     tags: ["Blockchain", "Aduting", "Solidity", "Ethereum"],
-    date: "Dec'23 - Present"
+
+  },
+  {
+    id: 3,
+    name: "Volyrix",
+    description:
+      "Frontend & Blockchain Developer.",
+    tags: ["Blockchain", "NextJS", "MERN", "NFT"],
 
   },
   {
     id: 4,
-    name: "UETH Bengaluru Chapter",
-    description:
-      " Chapter Leader",
-    tags: ["Blockchain", "Commuinty", "bengaluru", "Ethereum"],
-    date: "Nov'23 - Present"
-
-  },
-  {
-    id: 5,
-    name: "Web3Bengaluru",
-    description:
-      " Commuinty Head & Web3 Developer",
-    tags: ["Blockchain", "Commuinty", "bengaluru", "polygon"],
-    date: "Oct'23 - Present"
-
-  },
-  {
-    id: 6,
-    name: "ASHRUNYA",
-    description:
-      "Front End & Blockchain Developer.",
-    tags: ["Blockchain", "Reactjs", "Tailwindcss", "NFT"],
-    date: "July'23 - Present"
-
-
-  },
-  {
-    id: 7,
-    name: "Freelance",
-    description:
-      "MERN,Blockchain Developer & Frontend Flutter Development",
-    tags: ["Blockchain", "MERN", "Flutter"],
-    date: "May'23 - Present"
-
-  },
-  {
-    id: 8,
     name: "IoTree Minds",
     description:
       "FullStack Android Developer & Frontend Web Developer - Internship",
     tags: ["MERN", "Flutter", "TeamCollaboration"],
-    date: "Nov'22 - Jan'23"
-
-  },
-  {
-    id: 9,
-    name: "Zakthi Innovation Labs LLP",
-    description:
-      "Web Developer & App Development Trainer - Freelance",
-    tags: ["WebDev", "AppDev", "Training"],
-    date: "Nov '22 - March '23"
 
   },
 ];
